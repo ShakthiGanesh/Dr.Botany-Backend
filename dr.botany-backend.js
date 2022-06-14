c2bee2a94d1ae1544bc60018d7c3575ddfe9d386
shakthi ganesh <shakthi012000@gmail.com>
2022-06-14 12:35:53 +0530

    Add cors

--- dr.botany-backend.js
+++ dr.botany-backend.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://dr-botany3.web.app"],
         credentials: true,
     })
 );
